1a6306d2d74a2384d7a4f9415e8122efcc73652f
Pedro <ppaulo030601@gmail.com>
2020-08-20 14:33:03 -0300

    Bomb, Fire, Ice arrow addeds
    Functional Bomb Arrow, Ice and Fire only Particle System Ready

---
+++ Plataformer/Assets/Scripts/Arrow/BombArrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombArrow : Arrow
+{
+    public GameObject explosion;
+    public Collider2D explosionArea;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+       Explode();
+    }
+
+    public void Explode(){
+       GameObject clone = Instantiate(explosion, transform.position, Quaternion.Euler (Vector3.zero));
+       explosionArea.enabled = true;
+       Destroy(gameObject);
+   }
+
+   private void OnTriggerEnter2D(Collider2D other){
+       if(other.gameObject.CompareTag("Enemy")){
+           other.gameObject.GetComponent<Enemy>().Hurt(Vector3.zero);
+       }
+   }
+}
---
+++ Plataformer/Assets/Scripts/Arrow/FireArrow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireArrow : Arrow
+{
+    public GameObject particles;
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Enemy")){
+            Destroy(gameObject);
+            other.gameObject.GetComponent<Enemy>().Hurt(myRigidbody.velocity.normalized * knockBackForce);
+        }
+        else{
+            myRigidbody.velocity = Vector2.zero;
+            Destroy(particles);
+        }
+
+    }
+}
